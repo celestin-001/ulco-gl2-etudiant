78ae5fc9f981fd60050eab629e5b3a72c1d7a234
TODO <TODO>
2024-02-21 17:04:33 +0100

    C++ -> Python

--- tp-ffi/py-fibo2/cpp/myfibo.cpp
+++ tp-ffi/py-fibo2/cpp/myfibo.cpp
@@ -5,7 +5,18 @@ int fiboNaive(int n) {
     return n < 2 ? n : fiboNaive(n-1) + fiboNaive(n-2);
 }
 
-// TODO implement fiboIterative
+
+int fiboIterative(int n){
+    assert(n >+0);
+    int fibo_1=1;
+    int fibo_2 = 0;
+    for(int i=0; i<n; i++){
+        int cmpt = fibo_1;
+        fibo_1+=fibo_2;
+        fibo_2 = cmpt;
+    }
+    return fibo_2;
+}
 
 
 #include <pybind11/pybind11.h>
@@ -13,6 +24,9 @@ int fiboNaive(int n) {
 PYBIND11_MODULE(myfibo, m) {
 
     // TODO export fiboNaive (as fibo_naive)
+    m.doc() = "pybind11 example plugin";
+    m.def("fibo_naive",&fiboNaive);
+    m.def("fibo_iterative",&fiboIterative);
 
     // TODO export fiboIterative (as fibo_iterative)
 
--- tp-ffi/py-fibo2/scripts/main1.py
+++ tp-ffi/py-fibo2/scripts/main1.py
@@ -1,9 +1,12 @@
-
+import myfibo
 if __name__ == '__main__':
 
     print('TODO')
     
-    # TODO print fibo_naive(i) for i from 0 to 10
+    
+    for i in range(11):
+        print("fibo_naive("+str(i)+") = "+ str(myfibo.fibo_naive(i)))
 
-    # TODO print fibo_iterative(i) for i from 0 to 10
+    for i in range(11):
+        print("fibo_iterative("+str(i)+") = "+ str(myfibo.fibo_iterative(i)))
 
