6f6414ce6ff9a86e627c4d9c0b582eb00276005a
TODO <TODO>
2024-02-21 16:57:02 +0100

    Fibonaive et FiboIterative

--- tp-ffi/py-fibo1/mypackage/myfibo.py
+++ tp-ffi/py-fibo1/mypackage/myfibo.py
@@ -3,5 +3,13 @@ def fibo_naive(n: int):
     assert n >= 0
     return n if n < 2 else fibo_naive(n-1) + fibo_naive(n-2)
 
-# TODO implement fibo_iterative
 
+def fibo_iterative(n: int):
+    assert n>=0
+    fibo_1 = 1
+    fibo_2 = 0
+    for i in range(n):
+        tmp=fibo_1
+        fibo_1=fibo_1+fibo_2
+        fibo_2=tmp
+    return fibo_2
