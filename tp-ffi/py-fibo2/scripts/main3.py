10e51e11d96d41efaae306991b6d9b2e66a5b6ee
TODO <TODO>
2024-02-21 17:13:32 +0100

    SpinButon main3.py

--- tp-ffi/py-fibo2/scripts/main3.py
+++ tp-ffi/py-fibo2/scripts/main3.py
@@ -6,6 +6,7 @@ from matplotlib.backends.backend_gtk3agg import (
     FigureCanvasGTK3Agg as FigureCanvas)
 from matplotlib.figure import Figure
 import numpy as np
+import myfibo
 
 
 class Gui(Gtk.Window):
@@ -53,7 +54,11 @@ class Gui(Gtk.Window):
 
     def update_canvas(self, n):
         self.ax.clear()
-        # TODO plot fibo_iterative
+        xs = np.arange(n+1)
+        ys = np.vectorize(myfibo.fibo_iterative)(xs)
+        self.ax.plot(xs, ys)
+        self.ax.set(xlabel='i', ylabel='test_func(i)')
+        self.ax.grid()
         self.canvas.draw()
 
 
