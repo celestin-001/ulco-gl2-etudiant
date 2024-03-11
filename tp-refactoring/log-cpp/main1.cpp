0d9b6e7dc4df635e06f0da2469f692a46379ae2c
TODO <TODO>
2024-03-11 15:43:57 +0100

    injection dependances C++

--- tp-refactoring/log-cpp/main1.cpp
+++ tp-refactoring/log-cpp/main1.cpp
@@ -18,11 +18,26 @@ int mycompute(int v0) {
     return v2;
 }
 
+int mycomputeFile(int v0) {
+    std::ofstream ofs("log.txt",std::ofstream::out);
+    ofs << "add3 " + std::to_string(v0) << std::endl;
+    const int v1 = add3(v0);
+    ofs << "mul2 " + std::to_string(v1) << std::endl;
+    const int v2 = add3(v1);
+    ofs.close();
+    return v2;
+}
+
+
+
 int main() {
     std::cout << "this is log-cpp" << std::endl;
 
     const int res = mycompute(18);
     std::cout << res << std::endl;
+
+    const int resF = mycomputeFile(18);
+    std::cout << resF << std::endl;
 
     return 0;
 }
--- tp-refactoring/log-cpp/main2.cpp
+++ tp-refactoring/log-cpp/main2.cpp
@@ -12,8 +12,39 @@ int mul2(int n) {
     return n*2;
 }
 
+int mycomputeFile(logFunc_t logF,int v0) {
+    
+    logF("add3 " + std::to_string(v0) + "\n");
+    const int v1 = add3(v0);
+    logF("mul2 " + std::to_string(v1)+ "\n");
+    const int v2 = add3(v1);
+    return v2;
+}
+
+void f1(const std::string & str){
+    std::cout<<str;
+}
+
 int main() {
     std::cout << "this is log-cpp" << std::endl;
+
+    logFunc_t f1 = [](const std::string & str){
+        std::cout<<str;
+    };
+
+    int res = mycomputeFile(f1, 18);
+    std::cout<< res <<std::endl;
+
+    std::ofstream ofs("log2F.txt");
+    logFunc_t f2 = [&ofs](const std::string & str){
+        ofs <<str;
+    };
+
+    int res2 = mycomputeFile(f2, 18);
+    std::cout<< res2 <<std::endl;
+
+
+
 
     return 0;
 }
