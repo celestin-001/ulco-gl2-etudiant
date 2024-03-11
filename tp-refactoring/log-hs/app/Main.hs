285513d4eb575805c39f7dd7fa88a565f7e331f9
TODO <TODO>
2024-03-11 14:53:18 +0100

    injonction de dependances Haskell

--- tp-refactoring/log-hs/app/Main.hs
+++ tp-refactoring/log-hs/app/Main.hs
@@ -1,3 +1,5 @@
+import System.IO
+
 add3 :: Int -> Int 
 add3 = (+3)
 
@@ -12,9 +14,39 @@ mycompute v0 = do
     let v2 = mul2 v1
     return v2
 
+mycomputeFile :: Int -> IO Int
+mycomputeFile v0 = do
+    h <- openFile "log.txt" WriteMode
+    hPutStrLn h ("add3 " ++ show v0)
+    let v1 = add3 v0
+    hPutStrLn h ("mul2 " ++ show v1)
+    let v2 = mul2 v1
+    hClose h
+    return v2
+
+
+mycomputeF ::(String -> IO()) -> Int -> IO Int
+mycomputeF logf v0 = do
+    logf ("add3 " ++ show v0)
+    let v1 = add3 v0
+    logf ("mul2 " ++ show v1)
+    let v2 = mul2 v1
+    return v2
+
 main :: IO ()
 main = do
     putStrLn "this is log-hs"
     
     res <- mycompute 18
     print res
+
+    resFile <- mycompute 18
+    print resFile
+
+    resFileF <- mycomputeF putStrLn 18
+    print resFileF
+
+    hF <- openFile "logF.txt" WriteMode
+    resF <- mycomputeF(\str ->hPutStrLn hF str) 18
+    hClose hF
+    print resF
